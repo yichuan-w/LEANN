77d7b60a611b490e01d8e29a8c4e20aabc6cb223
yichuan520030910320 <yichuan_wang@berkeley.edu>
2025-08-05 23:11:09 -0700

    feat: Add graph partition support for DiskANN backend
    - Add GraphPartitioner class for advanced graph partitioning
    - Add partition_graph_simple function for easy-to-use partitioning
    - Add pybind11 dependency for C++ executable building
    - Update __init__.py to export partition functions
    - Include test scripts for partition functionality
    
    The partition functionality allows optimizing disk-based indices
    for better search performance and memory efficiency.

--- packages/leann-backend-diskann/leann_backend_diskann/__init__.py
+++ packages/leann-backend-diskann/leann_backend_diskann/__init__.py
@@ -1,1 +1,12 @@
 from . import diskann_backend as diskann_backend
+from . import graph_partition
+
+# Export main classes and functions
+from .graph_partition import GraphPartitioner, partition_graph
+
+__all__ = [
+    "diskann_backend",
+    "graph_partition", 
+    "GraphPartitioner",
+    "partition_graph"
+]
